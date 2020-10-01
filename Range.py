f8a9ddf9f7caa51fc6c08ac937d691025132a742
charmip09 <71674381+charmip09@users.noreply.github.com>
2020-10-01 07:51:00 -0700

    Delete Range.py

--- Range.py
+++
@@ -1,8 +0,0 @@
-'''
-Write a program in Python which will find all such numbers which are divisible
-by 7 but are not a multiple of 5, between 2000 and 3200.
-'''
-
-print(*[x for x in range(2000, 3201) if not(x%7) and x%5])
-
-
